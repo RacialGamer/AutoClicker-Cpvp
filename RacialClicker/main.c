ed8956e6a929b0e9f6606fc80d4543f04b43355b
RacialGamer <140088807+RacialGamer@users.noreply.github.com>
2023-08-12 10:57:44 +0200

    Cleaner code

--- RacialClicker/main.c
+++ RacialClicker/main.c
@@ -2,24 +2,25 @@
 #include <windows.h>
 
 
-int cps = 0;
-int delay = 1000;
-int keybind;
-int screen_width;
-int screen_height;
+int Cps = 0;
+int Delay = 1000;
+int Keybind;
+int ScreenWidth;
+int ScreenHeight;
 int x;
 int y;
 
-void Action(int x, int y)
+void action(int x, int y)
 {
     mouse_event(MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0);
     mouse_event(MOUSEEVENTF_RIGHTUP, x, y, 0, 0);
-    Sleep(delay);
+    Sleep(Delay);
     mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
     mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
 }
 
-int main() {
+int main() 
+{
     printf("    ____             _       ___________      __             \n");
     printf("   / __ \\____ ______(_)___ _/ / ____/ (_)____/ /_____  _____\n");
     printf("  / /_/ / __ `/ ___/ / __ `/ / /   / / / ___/ //_/ _ \\/ ___/\n");
@@ -28,37 +29,41 @@ int main() {
 
 
     printf("How much cps: ");
-    scanf("%d", &cps);
-    if (cps == 0)
+    scanf("%d", &Cps);
+    if (Cps == 0)
     {
-        printf("Invalid input info. \nrestarting the program...\n\n\n");
+        printf("Invalid input info.\n\nrestarting the program...\n\n\n");
         Sleep(5000);
         main();
     }
     printf("Delay between clicks (in milliseconds): ");
-    scanf("%d", &delay);
-    if (delay > 999)
+    scanf("%d", &Delay);
+    if (Delay > 999)
     {
-        printf("Invalid input info, maximum amount of delay is 999. \nrestarting the program...\n\n\n");
+        printf("Invalid input info, maximum amount of delay is 999.\n\nrestarting the program...\n\n\n");
         Sleep(5000);
         main();
     }
     printf("Keybind Code: ");
-    scanf("%x", &keybind);
-    if (GetKeyState(keybind) == 0) {
-        printf("Invalid input info. \nCheck https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes for valid inputs. \nBy example: 0x11");
+    scanf("%x", &Keybind);
+    if (GetKeyState(Keybind) == 0) 
+    {
+        printf("Invalid input info. \nCheck https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes for valid inputs. By example: 0x11\n\nrestarting the program...\n\n\n");
+        Sleep(5000);
+        main();
     }
     
-    screen_width = GetSystemMetrics(SM_CXSCREEN);
-    screen_height = GetSystemMetrics(SM_CYSCREEN);
-    x = screen_width / 2;
-    y = screen_height / 2;
+    ScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+    ScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+    x = ScreenWidth / 2;
+    y = ScreenHeight / 2;
 
-    while (1) {
-        if ((GetKeyState(keybind) & 0x8000) != 0)
+    while (1) 
+    {
+        if ((GetKeyState(Keybind) & 0x8000) != 0)
         {
-            Action(x, y);
-            Sleep(1000 / cps - delay);
+            action(x, y);
+            Sleep(1000 / Cps - Delay);
         }
     }
 }
