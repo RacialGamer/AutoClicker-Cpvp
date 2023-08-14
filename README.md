386a066ab20628f8fde385f683bc0256c1013c5f
Bram1903 <70259613+Bram1903@users.noreply.github.com>
2023-08-14 12:19:00 +0200

    Update README and refactor autoclicker code
    The README file has been updated with more detailed and organized information about the 'RacialClicker' project. Autoclicker logic was refactored using 'SharpHook' library. The aim of this refactor is to improve functionality and readability of the code. Now, any key on the keyboard can serve as a toggle key. Additionally, the autoclicker has a more advanced random delay feature in order to achieve a more realistic clicking rate. Also, the README now includes instructions on running and using the application. Project and solution files have been renamed due to directory restructure.

--- README.md
+++ README.md
@@ -1,21 +1,39 @@
 # RacialClicker
-![GitHub release (latest by date)](https://img.shields.io/github/v/release/RacialGamer/AutoClicker-Cpvp?style=plastic)
-![GitHub all releases](https://img.shields.io/github/downloads/RacialGamer/AutoClicker-Cpvp/total?style=plastic)
 
-An autoclicker made in C# for cpvp.
+RacialClicker is a simple auto-clicker application written in C# that simulates mouse clicks at a given rate. It allows users to define a target clicks per second (CPS) and toggle the auto-clicker functionality using a specific keyboard key.
 
-# Features
-- Clicks the cps you specify on both mouse buttons
-- Small delay between the mouse button clicks which you can specify
-- Bind it to a keybind with [Virtual Key Codes](https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes/ "Virtual Key Codes"), by example 0x11 for Left Ctrl
-- Hold the keybind to start autoclicking
+## Features
 
-# How to use 
-1. Go to [releases](https://github.com/RacialGamer/AutoClicker-Cpvp/releases/latest/ "Latest Release")
-2. Download the zip
-3. Extract it and run RacialClicker.exe
-4. If it doesn't run and only opens terminal for a split second download [.NET 6.0](https://dotnet.microsoft.com/en-us/download/ ".NET 6.0")
+- Simulate mouse clicks at a user-defined rate.
+- Toggle the auto-clicker on/off using a customizable keyboard key.
+- Automatically calculates a CPS range to ensure a realistic clicking rate.
 
+## Compatibility
 
- *Note: When starting RacialClicker.exe a Blue Window Will appear most of the time.* \
- To run the application you have to press **More Info** and then **Run Anyways**.
+RacialClicker is designed to work on the following platforms:
+
+- **Windows**
+- **macOS** (including Apple Silicon support)
+- **Linux**
+
+## Getting Started
+
+1. Clone this repository to your local machine.
+2. Open the solution in your preferred C# development environment (e.g., Visual Studio, Visual Studio Code with appropriate extensions).
+3. Build the project to compile the application.
+
+## Usage
+
+1. Run the compiled executable (`RacialClicker.exe` on Windows, `RacialClicker` on macOS/Linux).
+2. The application will prompt you to enter your target CPS (clicks per second).
+3. Once your target CPS is set, press any key to define the toggle key for enabling/disabling the auto-clicker.
+4. Press the chosen toggle key to start or stop the auto-clicking loop.
+5. The application will print messages indicating the status of the auto-clicker.
+
+## Disclaimer
+
+Please note that the term "RacialClicker" used in this project's title is unrelated to any sensitive or offensive contexts. The name is used merely as an example for illustrative purposes.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/Program.cs
@@ -0,0 +1,124 @@
+﻿using SharpHook;
+using SharpHook.Native;
+
+namespace RacialClicker;
+
+/// <summary>
+///     Represents an abstract auto-clicker class that simulates mouse clicks at a given rate.
+/// </summary>
+internal abstract class AutoClicker
+{
+    private static int _targetCps; // The target clicks per second (CPS).
+    private static int _minDelay; // Minimum delay between clicks.
+    private static int _maxDelay; // Maximum delay between clicks.
+    private static bool _autoClickerEnabled; // Flag to indicate if the auto-clicker is enabled.
+    private static readonly EventSimulator Simulator = new(); // Simulates mouse events.
+    private static readonly TaskPoolGlobalHook Hook = new(); // Handles global keyboard input.
+    private static readonly Random Random = new(); // Generates random numbers.
+    private static KeyCode _toggleKey; // Keyboard key to toggle the auto-clicker.
+    private static bool _keyHasBeenSet; // Flag to indicate if the toggle key has been set.
+
+    /// <summary>
+    ///     The entry point of the application.
+    /// </summary>
+    private static void Main()
+    {
+        PrintLabel();
+
+        _targetCps = GetTargetCps(); // Get the target CPS from user input.
+        Console.WriteLine("The CPS range is between {0} and {1}.", _targetCps - 2, _targetCps + 2);
+
+        _minDelay = 1000 / (_targetCps + 2); // Calculate minimum delay to achieve CPS range.
+        _maxDelay = 1000 / (_targetCps - 2); // Calculate maximum delay to achieve CPS range.
+
+        Hook.KeyPressed += OnKeyPressed; // Subscribe to the global key press event.
+
+        Console.WriteLine("Press any key to set the toggle key...");
+
+        Hook.Run(); // Start the global keyboard hook.
+        Hook.Dispose(); // Dispose of the hook when done.
+    }
+
+    /// <summary>
+    ///     Retrieves the target clicks per second (CPS) from the user.
+    /// </summary>
+    /// <returns>The target CPS value.</returns>
+    private static int GetTargetCps()
+    {
+        Console.Write("Enter your target CPS: ");
+        if (int.TryParse(Console.ReadLine(), out var cps) && cps > 0)
+            return cps;
+
+        Console.WriteLine("Invalid input. Using default target CPS of 12.");
+        return 12; // Default value
+    }
+
+    /// <summary>
+    ///     Handles the global key press event.
+    /// </summary>
+    /// <param name="sender">The event sender.</param>
+    /// <param name="args">The event arguments containing the keyboard input data.</param>
+    private static void OnKeyPressed(object? sender, KeyboardHookEventArgs args)
+    {
+        if (!_keyHasBeenSet) // Check if the toggle key hasn't been set yet.
+        {
+            _toggleKey = args.Data.KeyCode;
+            _keyHasBeenSet = true; // Set the flag indicating that the key has been set.
+            Console.WriteLine($"Press {_toggleKey} to toggle the auto clicker.");
+        }
+        else if (args.Data.KeyCode == _toggleKey) // Toggle the auto-clicker.
+        {
+            _autoClickerEnabled = !_autoClickerEnabled;
+
+            if (_autoClickerEnabled)
+            {
+                Console.WriteLine("Auto clicker has been enabled.");
+                ThreadPool.QueueUserWorkItem(AutoClickLoop); // Start the auto-click loop.
+            }
+            else
+            {
+                Console.WriteLine("Auto clicker has been disabled.");
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Simulates the auto-click loop.
+    /// </summary>
+    /// <param name="state">The state object (unused).</param>
+    private static void AutoClickLoop(object? state)
+    {
+        while (_autoClickerEnabled)
+        {
+            PerformClick(); // Simulate a mouse click.
+            var randomDelay = Random.Next(_minDelay, _maxDelay); // Generate a random delay.
+            Thread.Sleep(randomDelay); // Pause execution for the generated delay.
+        }
+    }
+
+    /// <summary>
+    ///     Simulates a mouse click.
+    /// </summary>
+    private static void PerformClick()
+    {
+        Simulator.SimulateMousePress(MouseButton.Button1); // Simulate mouse button press.
+        Simulator.SimulateMouseRelease(MouseButton.Button1); // Simulate mouse button release.
+    }
+
+    /// <summary>
+    ///     Prints the application label/logo.
+    /// </summary>
+    private static void PrintLabel()
+    {
+        Console.WriteLine("""
+                          
+                              ____             _       ___________      __
+                             / __ \____ ______(_)___ _/ / ____/ (_)____/ /_____  _____
+                            / /_/ / __ `/ ___/ / __ `/ / /   / / / ___/ //_/ _ \/ ___/
+                           / _, _/ /_/ / /__/ / /_/ / / /___/ / / /__/ ,< /  __/ /
+                          /_/ |_|\__,_/\___/_/\__,_/_/\____/_/_/\___/_/|_|\___/_/
+                                                                                     
+                          
+                          """);
+    }
+}
