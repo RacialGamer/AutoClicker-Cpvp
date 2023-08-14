f99b53f7c226dcaf6d20f2d7ab399987163855c3
Bram1903 <70259613+Bram1903@users.noreply.github.com>
2023-08-14 23:20:26 +0200

    Refactor auto-clicker to simulate human-like clicking pattern
    The auto-clicker has been refactored to better simulate a human-being's clicking behavior.
    Instead of merely maintaining a fixed CPS, the application now generates a human-like clicking pattern using a random variation in the interval between clicks. The minimum and maximum delays variables have been removed and replaced with a timer (_clickTimer) and a stopwatch (ClickStopwatch) to measure click intervals. The PerformClick function now simulates variable click duration and a short pause between clicks has been added to further mimic a human's clicking pattern. Also, informative messages have been added in each section of the program to facilitate code readability and maintainability.

--- src/Program.cs
+++ src/Program.cs
@@ -1,48 +1,47 @@
-﻿using SharpHook;
+﻿using System.Diagnostics;
+using SharpHook;
 using SharpHook.Native;
 
 namespace RacialClicker;
 
 /// <summary>
-///     Represents an abstract auto-clicker class that simulates mouse clicks at a given rate.
+///     Represents an abstract auto clicker that simulates a human-like clicking pattern.
 /// </summary>
 internal abstract class AutoClicker
 {
-    private static int _targetCps; // The target clicks per second (CPS).
-    private static int _minDelay; // Minimum delay between clicks.
-    private static int _maxDelay; // Maximum delay between clicks.
-    private static bool _autoClickerEnabled; // Flag to indicate if the auto-clicker is enabled.
-    private static readonly EventSimulator Simulator = new(); // Simulates mouse events.
-    private static readonly TaskPoolGlobalHook Hook = new(); // Handles global keyboard input.
-    private static readonly Random Random = new(); // Generates random numbers.
-    private static KeyCode _toggleKey; // Keyboard key to toggle the auto-clicker.
-    private static bool _keyHasBeenSet; // Flag to indicate if the toggle key has been set.
+    private static int _targetCps;
+    private static bool _autoClickerEnabled;
+    private static readonly EventSimulator Simulator = new();
+    private static readonly TaskPoolGlobalHook Hook = new();
+    private static KeyCode _toggleKey;
+    private static bool _keyHasBeenSet;
+    private static Timer _clickTimer = null!;
+    private static readonly Stopwatch ClickStopwatch = new();
+    private static readonly Random Random = new();
+    private static readonly object ClickingLock = new(); // Lock object for synchronization
+    private static bool _isClicking;
 
     /// <summary>
-    ///     The entry point of the application.
+    ///     Entry point of the auto clicker application.
     /// </summary>
     private static void Main()
     {
         PrintLabel();
 
-        _targetCps = GetTargetCps(); // Get the target CPS from user input.
-        Console.WriteLine("The CPS range is between {0} and {1}.", _targetCps - 2, _targetCps + 2);
+        _targetCps = GetTargetCps(); // Set the target CPS from user input or default
+        Console.WriteLine("The target CPS is: {0}.", _targetCps);
 
-        _minDelay = 1000 / (_targetCps + 2); // Calculate minimum delay to achieve CPS range.
-        _maxDelay = 1000 / (_targetCps - 2); // Calculate maximum delay to achieve CPS range.
-
-        Hook.KeyPressed += OnKeyPressed; // Subscribe to the global key press event.
+        Hook.KeyPressed += OnKeyPressed; // Subscribe to key press events
 
         Console.WriteLine("Press any key to set the toggle key...");
 
-        Hook.Run(); // Start the global keyboard hook.
-        Hook.Dispose(); // Dispose of the hook when done.
+        Hook.Run(); // Start hooking keyboard events
+        Hook.Dispose(); // Release the hook when done
     }
 
     /// <summary>
-    ///     Retrieves the target clicks per second (CPS) from the user.
+    ///     Gets the target clicks per second (CPS) from the user.
     /// </summary>
-    /// <returns>The target CPS value.</returns>
     private static int GetTargetCps()
     {
         Console.Write("Enter your target CPS: ");
@@ -50,75 +49,104 @@ internal abstract class AutoClicker
             return cps;
 
         Console.WriteLine("Invalid input. Using default target CPS of 12.");
-        return 12; // Default value
+        return 12;
     }
 
     /// <summary>
-    ///     Handles the global key press event.
+    ///     Event handler for keyboard key presses.
     /// </summary>
-    /// <param name="sender">The event sender.</param>
-    /// <param name="args">The event arguments containing the keyboard input data.</param>
     private static void OnKeyPressed(object? sender, KeyboardHookEventArgs args)
     {
-        if (!_keyHasBeenSet) // Check if the toggle key hasn't been set yet.
+        if (!_keyHasBeenSet)
         {
-            _toggleKey = args.Data.KeyCode;
-            _keyHasBeenSet = true; // Set the flag indicating that the key has been set.
+            _toggleKey = args.Data.KeyCode; // Set the toggle key for auto clicker
+            _keyHasBeenSet = true;
             Console.WriteLine($"Press {_toggleKey} to toggle the auto clicker.");
         }
-        else if (args.Data.KeyCode == _toggleKey) // Toggle the auto-clicker.
+        else if (args.Data.KeyCode == _toggleKey)
         {
-            _autoClickerEnabled = !_autoClickerEnabled;
+            lock (ClickingLock)
+            {
+                _autoClickerEnabled = !_autoClickerEnabled; // Toggle auto clicker state
 
-            if (_autoClickerEnabled)
-            {
-                Console.WriteLine("Auto clicker has been enabled.");
-                ThreadPool.QueueUserWorkItem(AutoClickLoop); // Start the auto-click loop.
-            }
-            else
-            {
-                Console.WriteLine("Auto clicker has been disabled.");
+                if (_autoClickerEnabled)
+                {
+                    Console.WriteLine("Auto clicker has been enabled.");
+                    ClickStopwatch.Restart(); // Start stopwatch for measuring click intervals
+                    _clickTimer = new Timer(ClickTimerCallback, null, 0, 1); // Start the timer for click simulation
+                    _isClicking = true;
+                }
+                else
+                {
+                    Console.WriteLine("Auto clicker has been disabled.");
+                    _clickTimer.Dispose(); // Stop the click timer
+                    _isClicking = false;
+                }
             }
         }
     }
 
     /// <summary>
-    ///     Simulates the auto-click loop.
+    ///     Callback function for the click timer.
     /// </summary>
-    /// <param name="state">The state object (unused).</param>
-    private static void AutoClickLoop(object? state)
+    private static void ClickTimerCallback(object? state)
     {
-        while (_autoClickerEnabled)
+        lock (ClickingLock)
         {
-            PerformClick(); // Simulate a mouse click.
-            var randomDelay = Random.Next(_minDelay, _maxDelay); // Generate a random delay.
-            Thread.Sleep(randomDelay); // Pause execution for the generated delay.
+            if (!_isClicking) return;
+            PerformClick(); // Simulate a mouse click
+            _clickTimer.Change(CalculateClickInterval(), Timeout.Infinite); // Reschedule the timer for next click
         }
     }
 
     /// <summary>
-    ///     Simulates a mouse click.
+    ///     Calculates the interval between clicks based on the target CPS.
+    /// </summary>
+    private static int CalculateClickInterval()
+    {
+        var targetIntervalMs = 1000.0 / _targetCps; // Calculate desired interval between clicks
+        var randomVariation = Random.NextDouble() * 0.2 - 0.1; // Generate a random variation up to 10%
+
+        var adjustedIntervalMs = targetIntervalMs * (1.0 + randomVariation); // Apply random variation
+        return (int)Math.Max(adjustedIntervalMs, 1); // Ensure the interval is at least 1ms
+    }
+
+    /// <summary>
+    ///     Simulates a mouse click action.
     /// </summary>
     private static void PerformClick()
     {
-        Simulator.SimulateMousePress(MouseButton.Button1); // Simulate mouse button press.
-        Simulator.SimulateMouseRelease(MouseButton.Button1); // Simulate mouse button release.
+        double clickDurationMs = Random.Next(8, 12); // Simulate variable click duration
+        Simulator.SimulateMousePress(MouseButton.Button1); // Simulate mouse button press
+        Thread.Sleep((int)clickDurationMs); // Simulate click duration
+        PerformRelease(); // Release the mouse button after click
     }
 
     /// <summary>
-    ///     Prints the application label/logo.
+    ///     Simulates releasing the mouse button after a click.
+    /// </summary>
+    private static void PerformRelease()
+    {
+        Simulator.SimulateMouseRelease(MouseButton.Button1); // Simulate mouse button release
+
+        // Simulate a short pause between clicks
+        var interClickPauseMs = Random.Next(8, 20);
+        Thread.Sleep(interClickPauseMs);
+    }
+
+    /// <summary>
+    ///     Prints the ASCII art label for the auto clicker.
     /// </summary>
     private static void PrintLabel()
     {
         Console.WriteLine("""
                           
-                              ____             _       ___________      __
-                             / __ \____ ______(_)___ _/ / ____/ (_)____/ /_____  _____
-                            / /_/ / __ `/ ___/ / __ `/ / /   / / / ___/ //_/ _ \/ ___/
-                           / _, _/ /_/ / /__/ / /_/ / / /___/ / / /__/ ,< /  __/ /
-                          /_/ |_|\__,_/\___/_/\__,_/_/\____/_/_/\___/_/|_|\___/_/
-                                                                                     
-                          
+                                        ____             _       ___________      __
+                                       / __ \____ ______(_)___ _/ / ____/ (_)____/ /_____  _____
+                                      / /_/ / __ `/ ___/ / __ `/ / /   / / / ___/ //_/ _ \/ ___/
+                                     / _, _/ /_/ / /__/ / /_/ / / /___/ / / /__/ ,< /  __/ /
+                                    /_/ |_|\__,_/\___/_/\__,_/_/\____/_/_/\___/_/|_|\___/_/
+                                                                                                  
                           """);
     }
 }
